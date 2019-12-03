62f4a796e8356ec901673e840ae50987c3e5cb0a
AndrewKarst <56857467+AndrewKarst@users.noreply.github.com>
2019-12-02 20:41:09 -0600

    Add files via upload

---
+++ RPGDiceRoller.js
@@ -0,0 +1,53 @@
+//Event listener to calculate roll after the button is clicked.
+document.getElementById("letsroll").addEventListener("click", setupDice);
+
+//Function that calculates the roll based on type and number of dice selected.
+function setupDice() {
+	
+    var d4 = document.getElementById("D4");
+	var d6 = document.getElementById("D6");
+	var d8 = document.getElementById("D8");
+	var d10 = document.getElementById("D10");
+	var d12 = document.getElementById("D12");
+	var d20 = document.getElementById("D20");
+	var numDice = parseInt(document.getElementById("numdice").value, 10); //number of dice from user converted to an integer.
+	var modifier = parseInt(document.getElementById("modifier").value, 10); //modifier from user converted to an integer.
+	var totalDice = 0; //total of the dice roll
+	var result = "Result"; //total dice and modifiers to be output as a string.
+	
+	if (d4.checked == true) {
+		for (var i = 0; i < numDice; i++) {
+			totalDice += Math.round(Math.random() * 4);
+		}
+	}
+	else if (d6.checked == true) {
+		for (var i = 0; i < numDice; i++) {
+			totalDice += Math.round(Math.random() * 6);
+		}
+	}
+	else if (d8.checked == true) {
+		for (var i = 0; i < numDice; i++) {
+			totalDice += Math.round(Math.random() * 8);
+		}
+	}
+	else if (d10.checked == true) {
+		for (var i = 0; i < numDice; i++) {
+			totalDice += Math.round(Math.random() * 10);
+		}
+	}
+	else if (d12.checked == true) {
+		for (var i = 0; i < numDice; i++) {
+			totalDice += Math.round(Math.random() * 12);
+		}
+	}
+	else if (d20.checked == true) {
+		for (var i = 0; i < numDice; i++) {
+			totalDice += Math.round(Math.random() * 20);
+		}
+	}
+	
+	totalDice += modifier; //adds modifier to the total dice roll.
+
+	result = totalDice.toString(); //converts totalDice integer to a string.
+	document.getElementById("resultp").textContent = "You rolled " + result; //replaces content of result div with the roll.
+}
